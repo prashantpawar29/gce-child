d278e818516a64624d0055300043607882069b62
Prashant Pawar <pawarprashant.18@gmail.com>
2024-06-06 07:41:08 +0000

    Modified main.tf file to use tag v1.0

--- child-module/main.tf
+++ child-module/main.tf
@@ -9,7 +9,7 @@
 # }
 
 module "infra_mgmt_host" {
-  source = "git::https://github.com/prashantpawar29/tf-test.git//root-module?ref=root"
+  source = "git::https://github.com/prashantpawar29/gce-root.git//root-module?ref=v1.0"
   count  = length(var.infra_mgmt_host)
 
   /* global */
